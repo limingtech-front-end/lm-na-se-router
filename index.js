fd71eb64434cf6ea3d8c3cb25cbccadbfb89dc5d
limingtech <limingtech@hotmail.com>
2017-01-03 19:58:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import bridge from 'lm-na-bridge'
+
+function makeHybridRouterMethod(methodName) {
+    return function(bridgeConnection, paramsObj, success, fail) {
+    	try{
+	        bridgeConnection.callHandler(methodName, paramsObj || {}, function(response) {
+	            success && success()
+	        })    		
+    	}catch(err){
+    		alert(err)
+    		fail && fail(err)
+    	}
+    }
+}
+
+function executeHybridRouterMethod(options) {
+    return new Promise((resolve, reject) => {
+        bridge().then((bridgeConnection) => {
+            router[options.name](bridgeConnection, options.query || {}, resolve, reject)
+        }, reject)
+    })
+}
+
+let router = {
+    login: makeHybridRouterMethod('router_login'),
+    back: makeHybridRouterMethod('router_back')
+}
+
+
+
+export default {
+    push: executeHybridRouterMethod,
+    back() {
+        executeHybridRouterMethod({ name: 'back' })
+    }
+}
